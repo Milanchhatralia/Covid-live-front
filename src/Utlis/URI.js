5b4ba9a9144404d22eb9f5af12e68d026a85a1bb
holidifymilan <milan@holidify.com>
2020-07-12 19:30:25 +0530

    API urls

---
+++ src/Utlis/URI.js
@@ -0,0 +1,7 @@
+const covid19API = "https://covid-live.azurewebsites.net";
+
+const localAPI = "https://localhost:8181";
+
+const countryURI = "https://coronavirus-19-api.herokuapp.com/countries"
+
+export {covid19API, localAPI, countryURI}
