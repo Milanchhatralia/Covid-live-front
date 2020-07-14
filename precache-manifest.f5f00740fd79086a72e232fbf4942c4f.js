9c45f86db000d80c900b8fc8f38a23c2eea58873
holidifymilan <milan@holidify.com>
2020-07-14 09:38:24 +0530

    Updates

---
+++ precache-manifest.f5f00740fd79086a72e232fbf4942c4f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e117188344645c417907d8e52d5669ab",
+    "url": "/Covid-live-front/index.html"
+  },
+  {
+    "revision": "3e3ac100a1a5b6cac84f",
+    "url": "/Covid-live-front/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "f1f6947d687d4847d724",
+    "url": "/Covid-live-front/static/css/main.ec4f0843.chunk.css"
+  },
+  {
+    "revision": "3e3ac100a1a5b6cac84f",
+    "url": "/Covid-live-front/static/js/2.1f73152d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Covid-live-front/static/js/2.1f73152d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f6947d687d4847d724",
+    "url": "/Covid-live-front/static/js/main.30cfb9fd.chunk.js"
+  },
+  {
+    "revision": "043efc6810f2f990d3c7",
+    "url": "/Covid-live-front/static/js/runtime-main.aa0fe6a3.js"
+  },
+  {
+    "revision": "98489e988c028180d8785ca27b5c498f",
+    "url": "/Covid-live-front/static/media/covid-live.98489e98.svg"
+  },
+  {
+    "revision": "2500ad58390e7dadbb795d9cae662e3a",
+    "url": "/Covid-live-front/static/media/loading.2500ad58.svg"
+  }
+]);
