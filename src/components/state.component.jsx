37eced1d32c62272863a3f2d02e773837c803ac7
Milan Chhatralia <milan@holidify.com>
2021-04-28 23:59:50 +0530

    removed tested data in state

--- src/components/state.component.jsx
+++ src/components/state.component.jsx
@@ -4,56 +4,51 @@ import Loader from '../components/elements/loader.component';
 import { numAbb } from '../Utlis/numAbb';
 // import { isDay } from '../Utlis/dayNight';
 
-const State = ({state, statecode, active, confirmed, recovered, deceased, deltaconfirmed, deltarecovered, deltadeceased, tested, testperthousand, isStateLoading})=>{
+const State = ({state, statecode, active, confirmed, recovered, deceased, deltaconfirmed, 
+    deltarecovered, deltadeceased, tested, vaccinated, percent_vaccinated, testperthousand, isStateLoading})=>{
         return(
-            <Col md={12} lg={8} className="px-2 mt-3">
+            <Col md={12} lg={6} className="px-2 mt-3">
                 <div className="cl-regionCard">
                     <p className="cl-regionName">{state} &#8226; {statecode}</p>
-                    { tested > 0 ? (<span className="cl-tested">Tested: <span>{numAbb(tested)}</span></span>) : ''}
+                    { tested > 0 ? (<span className="cl-card-chip">Vaccinated: <span>{numAbb(vaccinated)}</span></span>) : ''}
                     
                     <Loader isLoading={isStateLoading}/>
                     <Row className="m-0">
                         <Col xs={6} lg={4} className="cl-dataContainer cl-active mt-3">
                             <span>Active</span>
-                            <h4 style={dataColor(isStateLoading)}>
+                            <h4>
                                 { numAbb(active) }
                             </h4>
                         </Col>
                         <Col xs={6} lg={4} className="cl-dataContainer cl-confirmed mt-3">
                             <span>Confirmed</span>
-                            <h4 style={dataColor(isStateLoading)}>
+                            <h4>
                                 { numAbb(confirmed) }
                                 { deltaconfirmed > 0 ?<span className="delta"> &#8593;{deltaconfirmed}</span> : ''}
                             </h4>
                             
                         </Col>
-                        <Col xs={6} lg={4}className="cl-dataContainer cl-testesperkilo mt-3 d-none d-md-block">
-                            <span>Tested</span>
-                            <h4 style={dataColor(isStateLoading)}>
-                                ≈ { testperthousand }
-                                <span className="delta"> /1000</span>
-                            </h4>
-                        </Col>
                         <Col xs={6} lg={4} className="cl-dataContainer cl-recovered mt-3">
                             <span>Recovered</span>
-                            <h4 style={dataColor(isStateLoading)}>
+                            <h4>
                                 { numAbb(recovered) }
                                 { deltarecovered > 0 ?<span className="delta"> &#8593;{deltarecovered}</span> : ''}
                             </h4>
                         </Col>
                         <Col xs={6} lg={4} className="cl-dataContainer cl-deceased mt-3">
                             <span>Deceased</span>
-                            <h4 style={dataColor(isStateLoading)}>
+                            <h4>
                                 { numAbb(deceased) }
                                 { deltadeceased > 0 ?<span className="delta"> &#8593;{numAbb(deltadeceased)}</span> : ''}
                             </h4>
                         </Col>
-                        <Col xs={6} lg={4} className="cl-dataContainer cl-testesperkilo mt-3 d-block d-md-none">
-                            <span>Tested</span>
-                            <h4 style={dataColor(isStateLoading)}>
-                                ≈ { testperthousand }
-                                <span className="delta"> /1000</span>
+                        <Col xs={6} lg={4} className="cl-dataContainer cl-vaccinated mt-3">
+                            <span>Vaccinated</span>
+                            <h4>
+                                ≈ { percent_vaccinated }%
+                                <span className="delta"> &nbsp;{ numAbb(vaccinated)}</span>
                             </h4>
+                            <span style={pseudoLine(percent_vaccinated)} className="-pseudo-line"></span>
                         </Col>
                     </Row>
                     
@@ -62,10 +57,11 @@ const State = ({state, statecode, active, confirmed, recovered, deceased, deltac
         )
     
 }
-const dataColor = isLoading => {
-    let loading = isLoading? '#fffed1' : '';
-    return{ 
-        color: loading,
+
+const pseudoLine = percent_vaccinated => {
+    percent_vaccinated = Math.round(percent_vaccinated);
+    return{
+        backgroundImage: `linear-gradient(90deg, #3f914e ${percent_vaccinated}%, #e9fae9 ${percent_vaccinated}%, #e9fae9 100%)`,
     }
 }
 
