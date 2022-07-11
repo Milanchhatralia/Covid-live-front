56eed33dcc2f767122d34377af7f3e4e80da7ea0
Amith Tiwari <amitht18@gmail.com>
2022-07-11 18:00:16 +0530

    Import fonts and fix icon behaviour

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -5,8 +5,8 @@ import { fetchRegionDetail } from '../Utlis/getCovidData'
 import { debounce } from '../Utlis/optimize';
 import { fetchCovidStateData, fetchCovidCountryData } from '../Utlis/getCovidData';
 
-export default class Navbar extends Component{
-    constructor(props){
+export default class Navbar extends Component {
+    constructor(props) {
         super(props)
         this.state = {
             focused: false,
@@ -15,7 +15,7 @@ export default class Navbar extends Component{
         this.handleInputThrottled = debounce(this.updateSearchResult, 300)
     }
 
-    updateSearchResult(searchStr){
+    updateSearchResult(searchStr) {
         fetchRegionDetail(searchStr)
             .then(data => {
                 this.setState({ searchResult: data })
@@ -28,38 +28,38 @@ export default class Navbar extends Component{
     }
 
     onBlur = () => {
-        setTimeout(()=>{
+        setTimeout(() => {
             this.setState({ focused: false })
-        },200)
+        }, 200)
     }
 
     regionClick = (e) => {
         const { searchResult } = this.state;
-        const {changeCity, changeState, changeCountry} = this.props;
+        const { changeCity, changeState, changeCountry } = this.props;
         let index = [...e.target.parentElement.children].indexOf(e.target);
         let item = searchResult[index];
         if (item.type === 'city') {
             changeCity(item);
-            if(item.state !== 'undefined') {
+            if (item.state !== 'undefined') {
                 fetchCovidStateData(item.state).then((res) => {
                     changeState(res.data);
-                }).catch((err)=>{
+                }).catch((err) => {
                     console.log(err);
                 });
             }
-            if(item.country){
+            if (item.country) {
                 fetchCovidCountryData(item.country).then((res) => {
                     changeCountry(res.data);
-                }).catch((err)=>{
+                }).catch((err) => {
                     console.log(err);
                 });
             }
-        }else{
+        } else {
             changeState(item);
-            if(item.country){
+            if (item.country) {
                 fetchCovidCountryData(item.country).then((res) => {
                     changeCountry(res.data);
-                }).catch((err)=>{
+                }).catch((err) => {
                     console.log(err);
                 });
             }
@@ -71,34 +71,39 @@ export default class Navbar extends Component{
         let { searchResult } = this.state;
         let listItems = searchResult.map((item) =>
             item.type === 'city'
-            ? <li className='cl-regionItem'>
-                { item.city }
-                { item.state !== 'undefined' ? <span className="cl-region-code text-capitalize">{item.state}</span> :""}
-            </li>
-            : <li className='cl-regionItem'>
-                { item.state }
-                { item.countrycode !== 'undefined' ? <span className="cl-region-code text-uppercase">{item.countrycode}</span> :""}
-            </li>
+                ? <li className='cl-regionItem'>
+                    {item.city}
+                    {item.state !== 'undefined' ? <span className="cl-region-code text-capitalize">{item.state}</span> : ""}
+                </li>
+                : <li className='cl-regionItem'>
+                    {item.state}
+                    {item.countrycode !== 'undefined' ? <span className="cl-region-code text-uppercase">{item.countrycode}</span> : ""}
+                </li>
         );
-    
+
         return (
             <ul onClick={this.regionClick}>{listItems.length > 0 ? listItems : <li className='cl-regionItem'>No region found</li>}</ul>
         );
     }
 
-    render(){
+    render() {
         let { focused } = this.state;
-        return(
+        return (
             <React.Fragment>
-            <div className="cl-navbar">
-                <input id="cl-navSearchbox" type="text" placeholder="city, state or country" onKeyUp={e => this.handleInputThrottled(e.target.value)} onFocus={this.onFocus} onBlur={this.onBlur} autocomplete="off"/>
-                <label htmlFor="cl-navSearchbox" className="cl-searchIcon"><img  src={SearchIcon} alt=""/></label>
-                <Logo style={logoStyle}/>
-                <div style={searchResultStyle(focused)} className="cl-search-result">
-                    <this.resultList/>
+                <div className="cl-navbar">
+                    <div className="cl-logo">
+                        <Logo style={logoStyle} />
+                        <h4 className="cl-name">Covid Dashboard</h4>
+                    </div>
+                    <div className="cl-search-icon">
+                        <label htmlFor="cl-navSearchbox" className="cl-searchIcon"><img src={SearchIcon} alt="" /></label>
+                        <input id="cl-navSearchbox" type="text" placeholder="city, state or country" onKeyUp={e => this.handleInputThrottled(e.target.value)} onFocus={this.onFocus} onBlur={this.onBlur} autocomplete="off" />
+                    </div>
+                    <div style={searchResultStyle(focused)} className="cl-search-result">
+                        <this.resultList />
+                    </div>
                 </div>
-            </div>
-            
+
             </React.Fragment>
         )
     }
@@ -106,13 +111,13 @@ export default class Navbar extends Component{
 
 const searchResultStyle = (focused) => {
     let displayProp = focused ? "block" : "none";
-    return{
+    return {
         display: displayProp,
     }
 }
 const logoStyle = {
     height: '100%',
-    width: '50px',
+    width: '30px',
     marginLeft: '6px',
     transition: 'all 0.2s ease-in',
 }
